9b5df76ff171c0bfde0ac62955b605ee4105d92e
Rafa-angelino <rafaangelino22@gmail.com>
2023-07-23 20:38:30 -0300

    feat: criando breadcrumb , melhorando a product-details, adicionando loading e criando a pagina home com carousel

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ShopService } from '../shop.service';
 import { Product } from 'src/app/shared/models/product';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,10 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product?: Product;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute){}
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService){
+      this.bcService.set('@productDetails', ' '); //inicializa como vazio ao carregar a página para assim quando retornar da api ser inserido o nome
+    }
 
   ngOnInit(): void {
     this.loadProduct()
@@ -19,8 +23,12 @@ export class ProductDetailsComponent implements OnInit {
 
   loadProduct(){
     const id = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log('id', id)
     if(id) this.shopService.getProduct(+id).subscribe({
-      next: response => this.product = response,
+      next: productDetails => {
+        this.product = productDetails;
+        this.bcService.set('@productDetails', productDetails.name); //coloca o nome vindo da api no header
+      },
       error : err => console.log("Erro ao carregar produto por Id")
     });
   }
